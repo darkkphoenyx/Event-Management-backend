2db88be55c66a76abc06d49685844384e4120e8c
Prashant Rai <r56.prashant@gmail.com>
2024-01-25 07:49:08 +0545

    some changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import memberRouter from './routes/member.router'
 import adminRouter from './routes/admin.router'
-import { Request, Response, NextFunction } from 'express'
 import buildError from './utils/build-errors'
+import HttpStatus from 'http-status-codes'
+
+
 
 const app = express()
 
@@ -16,6 +18,19 @@ app.listen(PORT, () =>
 app.use('/todos', memberRouter)
 app.use('/admin', adminRouter)
 
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const error = buildError(err)
+    res.status(error.code).json({ error })
+})
+
+app.use(function METHOD_NOT_ALLOWED(req: Request, res: Response) {
+    res.status(HttpStatus.METHOD_NOT_ALLOWED).json({
+        error: {
+            code: HttpStatus.METHOD_NOT_ALLOWED,
+            message: HttpStatus.getStatusText(HttpStatus.METHOD_NOT_ALLOWED),
+        },
+    })
+})
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     const error = buildError(err)
     res.status(error.code).json({ error })
